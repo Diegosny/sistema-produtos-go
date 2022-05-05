6406e1bfebf0476349fd6e88d8d0037f4f3a2375
Diegosny <diegojamesmetalica@gmail.com>
2022-05-05 08:12:17 -0300

    Criando nova rota que será a rota de cadastro

--- app/controller/product.go
+++ app/controller/product.go
@@ -9,7 +9,7 @@ import (
 var temp = template.Must(template.ParseGlob("template/" +
 	"*.html"))
 
-func Index(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, _ *http.Request) {
 	products := models.ReturnDate()
 
 	err := temp.ExecuteTemplate(w, "Index", products)
@@ -18,9 +18,13 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func Store(w http.ResponseWriter, r *http.Request) {
+func Create(w http.ResponseWriter, _ *http.Request) {
 	err := temp.ExecuteTemplate(w, "Create", nil)
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func Store(_ http.ResponseWriter, _ *http.Request) {
+
 }
